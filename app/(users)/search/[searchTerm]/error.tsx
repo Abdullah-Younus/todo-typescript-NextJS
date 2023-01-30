42b2c33eb1a70c1d7ba086483ad9372efc96e26c
Abdullah Younus <abdullah1001k@gmail.com>
2023-01-30 17:06:18 +0500

    local changes

---
+++ app/(users)/search/[searchTerm]/error.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react'
+import { useEffect } from 'react';
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+    useEffect(() => {
+        console.log(error);
+
+    }, [error])
+
+    return (
+        <div>
+            <p>Something went wrong!</p>
+            <button onClick={() => reset()}>Reset Error Boundary</button>
+        </div>
+    )
+}
